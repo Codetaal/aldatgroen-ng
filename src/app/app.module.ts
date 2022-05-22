0508a6b64eaa50f3c39e7edeee0125a270761963
Bram van der Meij <bram@newstory.nl>
2022-05-22 20:12:52 +0200

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { LoaderComponent } from './components/loader/loader.component';
 import { MessageListComponent } from './pages/message/message-list/message-list.component';
 import { MessageDetailsComponent } from './pages/message/message-details/message-details.component';
 import { TimeagoModule } from 'ngx-timeago';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -36,6 +38,12 @@ import { TimeagoModule } from 'ngx-timeago';
     AppRoutingModule,
     HttpClientModule,
     TimeagoModule.forRoot(),
+    ServiceWorkerModule.register('./ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [
     {
